2b2692bb97412b33d9cc0352e6ca70434ac6eb29
Markus Ongyerth <ongy@ongy.net>
2017-09-01 11:14:03 +0200

    Add Direction key handling

--- bin/Main.hs
+++ bin/Main.hs
@@ -25,6 +25,8 @@ import Control.Exception (bracket_)
 import Shared
 
 import Control.Monad (forM_)
+import Text.XkbCommon.InternalTypes (Keysym, Direction(..))
+import Text.XkbCommon.KeysymPatterns
 
 
 data OutputState = OutputState
@@ -34,16 +36,18 @@ data OutputState = OutputState
 
 data CatRenderer = CatRenderer (Ptr Renderer) (Ptr Texture)
 
+data RotationState = RotationState CatRenderer Double Double
+
 renderOn :: Ptr Output -> Ptr Renderer -> IO a -> IO a
 renderOn output rend act = bracket_ 
     (makeOutputCurrent output)
     (swapOutputBuffers output)
     (doRender rend output act)
 
-frameHandler :: IORef OutputState -> IORef CatRenderer -> Double -> Ptr Output -> IO ()
-frameHandler ref catRef secs output = do
+frameHandler :: IORef OutputState -> IORef RotationState -> Double -> Ptr Output -> IO ()
+frameHandler ref stateRef secs output = do
     state <- readIORef ref
-    (CatRenderer rend tex) <- readIORef catRef
+    (RotationState (CatRenderer rend tex) xSpeed ySpeed) <- readIORef stateRef
     (width, height) <- effectiveResolution output
     -- The offsets on the x axis
     let xs = [-128 + xOffset state, xOffset state .. width]
@@ -55,9 +59,12 @@ frameHandler ref catRef secs output = do
     renderOn output rend $ withMatrix $ \matrix -> forM_ zs $ \(x, y) -> do
         getMatrix tex matrix (getTransMatrix output) (fromIntegral x) (fromIntegral y)
         renderWithMatrix rend tex matrix
-    let adjust = floor $ 128 * secs
+    let adjustX = floor $ xSpeed * secs
+    let adjustY = floor $ ySpeed * secs
 
-    modifyIORef ref (\(OutputState x y) -> OutputState ((x + adjust) `mod` 128) ((y + adjust) `mod` 128))
+    modifyIORef ref (\(OutputState x y) -> OutputState
+        ((x + adjustX) `mod` 128)
+        ((y + adjustY) `mod` 128))
 
 
 getCatRenderer :: Ptr Backend -> IO (CatRenderer)
@@ -66,17 +73,34 @@ getCatRenderer backend = do
     texture <- getCatTexture renderer
     pure $ CatRenderer renderer texture
 
-handleOutputAdd :: IORef CatRenderer -> Ptr Output -> IO FrameHandler
+getInitialState :: Ptr Backend -> IO RotationState
+getInitialState backend = do
+    cat <- getCatRenderer backend
+    pure $ RotationState cat 128 128
+
+handleOutputAdd :: IORef RotationState -> Ptr Output -> IO FrameHandler
 handleOutputAdd catRef _ = do
     ref <- newIORef (OutputState 0 0)
 
     pure $ frameHandler ref catRef
 
+handleKeyPress :: IORef RotationState -> Keysym -> Direction -> IO ()
+handleKeyPress ref sym (Direction dir) = if dir == 1
+    then let fun = case sym of
+                Keysym_Up ->    \(RotationState c x y) -> RotationState c x (y - 16)
+                Keysym_Down ->  \(RotationState c x y) -> RotationState c x (y + 16)
+                Keysym_Right -> \(RotationState c x y) -> RotationState c (x + 16) y
+                Keysym_Left ->  \(RotationState c x y) -> RotationState c (x - 16) y
+                _ -> id
+          in modifyIORef ref fun
+    else pure ()
+
 main :: IO ()
 main = do
-    catRef <- newIORef undefined
+    stateRef <- newIORef undefined
 
     launchCompositor ignoreHooks
-        { backendPostHook = \backend -> writeIORef catRef =<< getCatRenderer backend
-        , outputAddHook = handleOutputAdd catRef
+        { backendPostHook = \backend -> writeIORef stateRef =<< getInitialState backend
+        , outputAddHook = handleOutputAdd stateRef
+          , keyPressHook = handleKeyPress stateRef
         }
